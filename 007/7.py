879fbc38d69eac9c5a6a5dcaf30fb43842de35bb
Ankit Solanki <ankit@simulacra.in>
2008-12-10 17:20:30 +0530

    Optimized the solution for #7, thanks to the notes provided

--- 007/7.py
+++ 007/7.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import math
+
 """
 I’m generating an infinite series of prime numbers using
 a method I remember from the SICP lectures. Fun.
@@ -15,22 +17,23 @@ def get_primes():
         for prime in _primes:
             if num % prime == 0:
                 return False
-            if prime > (num / 2):
+            if prime > math.sqrt(num):
                 return True
     
-    _primes = [2] # Holds all prime numbers found so far
-
+    _primes = [2, 3, 5] # Holds all prime numbers found so far
 
     yield _primes[0]
+    yield _primes[1]
+    yield _primes[2]
 
-    num = _primes[0] + 1 
+    num = _primes[2] + 2
 
     while True:
         if is_prime(num, _primes):
             _primes.append(num)
             yield num
 
-	num = num + 2 # We’re interested in odd numbers only
+	num = num + 6 # All primes greater than 3 can be written in the form 6k+/-1.
 
 primes = get_primes()
 prime = None
